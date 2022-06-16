8dc40e41d16e163a2ad632f4093c04410ccb9460
Aseem Asthana <aseem@aseem.us>
2022-06-16 10:57:59 -0700

    One package.json for ui and server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const confService = app.get(ConfigService);
+
+  const port = confService.get('SERVER_PORT', '0.0.0.0');
+  console.log(`Server listening on port ${port}`);
+
+  await app.listen(port);
 }
 bootstrap();
